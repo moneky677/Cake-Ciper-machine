d9df3d850d9b3b3a48793ac519e0d00d2b3303d5
moneky677 <dojyaaan650@gmail.com>
2026-05-27 19:47:51 -0400

    Enable continuous ciphering and deciphering
    Refactor the ciphering logic to run in a loop, allowing multiple inputs.

--- code.py
+++ code.py
@@ -1,17 +1,18 @@
 print("Welcome to the Cake Cipher!!!")
-choice = input("Would you like to cipher or decipher?\nd/c\n").lower().strip()
-while choice not in ['d','c']:
-    print("Not an option, please try again")
-    choice = input("Would you like to cipher or decipher?\nd/c\n").lower().strip()
-message = input("Please enter your message\n>>>\n").strip()
-if choice == 'c':
-   table = str.maketrans(
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{]}\\|;:,<.>/?",
-        "mzqvnturswbxcpfyaokhdgijleEKCVDLBSNHFGMPOAWUQZRJTXIY3071986254;|/+.`[{<\\}]~>?-=^)@(#*:!$%,_&"
-    )
-else:
-    table = str.maketrans(
-        "mzqvnturswbxcpfyaokhdgijleEKCVDLBSNHFGMPOAWUQZRJTXIY3071986254;|/+.`[{<\\}]~>?-=^)@(#*:!$%,_&",
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{]}\\|;:,<.>/?"
-    )
-print(message.translate(table))
+while True:
+    choice = input("\nWould you like to cipher or decipher?\n\nd/c\n").lower().strip()
+    while choice not in ['d','c']:
+        print("Not an option, please try again")
+        choice = input("Would you like to cipher or decipher?\nd/c\n").lower().strip()
+    message = input("Please enter your message\n>>>\n").strip()
+    if choice == 'c':
+        table = str.maketrans(
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{]}\|;:,<.>/?",
+                "VbX4m3JFTe_K8&2|v/SQG:.n@{ID16Eaiy*9`\[,NCBzd$^r#>(f+)}A%thO5ZMY]jPHuwRos?xklqc;<LW~0p!=7g-U"
+        )
+    else:
+        table = str.maketrans(
+            "VbX4m3JFTe_K8&2|v/SQG:.n@{ID16Eaiy*9`\[,NCBzd$^r#>(f+)}A%thO5ZMY]jPHuwRos?xklqc;<LW~0p!=7g-U",
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{]}\|;:,<.>/?"
+        )
+    print("\n\n"+message.translate(table))
